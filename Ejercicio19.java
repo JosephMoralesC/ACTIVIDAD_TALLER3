dc0e65b025f3c10a77ba7cc34893def93de0c56c
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 16:52:43 -0500

    Create Ejercicio19.java

---
+++ Ejercicio19.java
@@ -0,0 +1,11 @@
+package ejercicio19;
+
+
+public class Ejercicio19 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
