8ae03b9f0a358c424c611b0d9d32770766f217f2
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 17:17:14 -0500

    Create CEjercicio10.java

---
+++ CEjercicio10.java
@@ -0,0 +1,12 @@
+package ejercicio10;
+
+
+public class Ejercicio10 {
+
+    
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
