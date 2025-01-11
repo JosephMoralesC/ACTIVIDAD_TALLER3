fb25fe92b8106ec7a1d3634347df5147fbbe4dc0
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 16:44:08 -0500

    Update and rename PUNTO18 to PUNTO18.java

---
+++ PUNTO18.java
@@ -0,0 +1,11 @@
+package ejercicio18;
+
+public class Ejercicio18 {
+
+    
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
