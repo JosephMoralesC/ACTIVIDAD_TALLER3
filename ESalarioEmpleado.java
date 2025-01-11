b2f663eb0e0861148e10d5b2d0f501955f2bb35a
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 17:52:57 -0500

    Create ESalarioEmpleado.java

---
+++ ESalarioEmpleado.java
@@ -0,0 +1,11 @@
+package ejercicio22;
+
+public class SalarioEmpleado {
+    String nombre;
+    double sBasico, sMensual, horas;
+    
+    
+    public void calcSalario() {
+        sMensual = sBasico * horas;
+    }
+}
