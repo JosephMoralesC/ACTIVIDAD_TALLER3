48a72e12c2993a998ae4ccdd1298dbf1983c7ef1
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 17:40:49 -0500

    Create DEjercicio7.java

---
+++ DEjercicio7.java
@@ -0,0 +1,12 @@
+package ejercicio7;
+
+
+public class Ejercicio7 {
+
+   
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
