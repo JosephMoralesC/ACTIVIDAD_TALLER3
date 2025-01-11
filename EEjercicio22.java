0496c1b441609b4b122cdb314d7f10b2c6d37504
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 17:47:25 -0500

    Create EEjercicio22.java

---
+++ EEjercicio22.java
@@ -0,0 +1,12 @@
+package ejercicio22;
+
+
+public class Ejercicio22 {
+
+    
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
