1411eed27debd9c5b3e310c36231a153b9b91ae2
JosephMoralesC <80478064+JosephMoralesC@users.noreply.github.com>
2025-01-11 18:14:15 -0500

    Create GFiguras.java

---
+++ GFiguras.java
@@ -0,0 +1,12 @@
+package figuras;
+
+
+public class Figuras {
+
+    
+    public static void main(String[] args) {
+        Dashboard dash = new Dashboard();
+        dash.setVisible(true);
+    }
+    
+}
